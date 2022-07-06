15bf7786210e66a8adf5cd751ca19ca4b5fa66df
Shailav Malik <malikshailav@gmail.com>
2022-07-06 22:20:52 +0530

    .exe files deleted

--- 36b-Class_Padding.cpp
+++ 36b-Class_Padding.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
+#pragma pack(1)
 using namespace std;
+
+class demo
+{
+   public:
+   char c;
+   int num;
+};
  
 int main() 
 {
-    
+    printf("%d",sizeof(demo));
  
     return 0; 
 }
